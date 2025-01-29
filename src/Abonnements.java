e26ea6f1ece413e8704bed9985105c1a021def12
Tim4455 <ts449392@fh-muenster.de>
2025-01-29 14:25:25 +0100

    Enums

--- src/Abonnements.java
+++ src/Abonnements.java
@@ -1,2 +1,18 @@
-public class Abonnements {
+enum Abonnements {
+    SAUNA("Sauna", 10),
+    PARKPLATZ("Parkplatz", 5),
+    DUSCHE("Dusche", 5),
+    BODYPUMP("Bodypump", 15),
+    INDOOR_CYCLING("Indoor Cycling", 15),
+    BODABALANCE("Bodabalance", 15),
+    LES_MILLS_GRIT("Les Mills Grit", 15),
+    BODYSTYLING("Bodystyling", 15),
+    RUECKEN_SOFT("Rücken Soft", 15),
+    RUECKEN_AKTIV("Rücken Aktiv", 15),
+    BAUCH_BEINE_PO("Bauch Beine Po", 15),
+    CARDIO_MEETS_WELLNESS("Cardio meets Wellness", 15);
+
+    Abonnements(String bezeichnung, double preis) {
+
+    }
 }
